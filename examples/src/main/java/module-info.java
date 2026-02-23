4a1431e903e2d8ce828edf8c52066e8109c06801
Michael Simons <michael@simons.ac>
2026-02-23 09:52:00 +0100

    feat: Add support for the Java modules system. (#1)
    Signed-off-by: Michael J. Simons <michael@simons.ac>

---
+++ examples/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module me.bechberger.util.femtocli.examples  {
+	requires me.bechberger.util.femtocli;
+
+	opens me.bechberger.femtocli.examples to me.bechberger.util.femtocli;
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module me.bechberger.util.femtocli  {
+	requires java.desktop;
+
+	exports me.bechberger.femtocli;
+	exports me.bechberger.femtocli.annotations;
+}
